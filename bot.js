e83e03c16427e3e54eb061f7a7094461994a705f
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-28 17:08:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
+var prefix = "d";
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : EX Clan`);
@@ -8,9 +8,20 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`%0 النفسية`,"http://twitch.tv/Dream")
+client.user.setGame(`الوقت كلسيف ان لم تقطعههء قطع جدككء`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
+
+client.on('guildMemberAdd', Sal => { //By Salto7#4595
+    var embed = new Discord.RichEmbed()
+    .setTitle('Welcome')
+    .setDescription('ارحب نورت **Welcome To __Magic__**')
+    .setColor('RANDOM')
+    .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
+    var channel =Sal.guild.channels.find('name', 'magic') // هنا حط اسم الروم الي تبيه يكتب فيه
+    if (!channel) return;
+    channel.send({embed : embed});
+    });
 
 
 
